c11c5678e7e8d99b7bfdb920ba3f42b38ab3695e
Pankaj Phartiyal <pankajphartiyal90@gmail.com>
2013-05-22 15:26:36 +0530

    resolve css conflicts between bootstrap and old css

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130520224240) do
+ActiveRecord::Schema.define(:version => 20130520214323) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -163,9 +163,8 @@ ActiveRecord::Schema.define(:version => 20130520224240) do
 
   create_table "storage_records", :force => true do |t|
     t.integer  "bin_number"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.boolean  "is_available"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "tiers", :force => true do |t|

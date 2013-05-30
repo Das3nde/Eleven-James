bfba89c606ff9a07a83b56d6d1ec52daff225f1f
Rohan Dey <rohandey@gmail.com>
2013-05-30 12:46:29 +0530

    signup html changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,7 +8,18 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    # add custom create logic here
+    build_resource
+    @resource = resource
+    @resource.name = params[:name]
+    @resource.email = params[:email]
+    @resource.password = params[:password]
+    @resource.password_confirmation = params[:confirm_password]
+    @resource.valid?
+    puts @resource.errors.full_messages.join("__")
+    if @resource.errors.keys.length == 0
+      @resource.save
+    end
+    render json: {done: "sf"}
   end
 
 

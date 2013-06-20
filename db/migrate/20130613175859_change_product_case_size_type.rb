86cd2b601925130a8878b7d1907d6bfe7c7398ef
Rohan Dey <rohandey@gmail.com>
2013-06-20 15:56:00 +0530

    comment migration to fix

--- db/migrate/20130613175859_change_product_case_size_type.rb
+++ db/migrate/20130613175859_change_product_case_size_type.rb
@@ -1,13 +1,16 @@
 class ChangeProductCaseSizeType < ActiveRecord::Migration
   def up
     #change_column :products, :case_size, :int, :using
+=begin
     Product.all.each do |p|
       p.case_size =  10 #change all to integer value
       p.save
     end
     ActiveRecord::Base.connection.execute("ALTER TABLE products ALTER COLUMN case_size TYPE integer USING (trim(case_size)::integer);")
+=end
   end
 
   def down
+    ActiveRecord::Base.connection.execute("ALTER TABLE products ALTER COLUMN case_size TYPE varchar USING (case_size::varchar);")
   end
 end

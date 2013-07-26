5fd184e63d5e8819b733a31d2d320c874b098c34
Rohan Dey <rohandey@gmail.com>
2013-07-26 18:18:16 +0530

    ipn for instant payment

--- app/controllers/ipn_controller.rb
+++ app/controllers/ipn_controller.rb
@@ -4,6 +4,15 @@ class IpnController < ApplicationController
 
   def index
     Notify.ipn(params).deliver
+
+    case params[:payment_type]
+    when 'instant'
+      user = User.where("username = ? ", params[:last_name]).first
+      payment = Payment.where("user_id = ? and purpose = 'signup'", user.id).first
+      payment.ipn_status = params[:payment_status]
+      payment.ipn_response_dump = params
+      payment.save
+    end
     render text: "IPN"
   end
 

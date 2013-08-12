c815785327f8f1efde11da2d856f5937ddcce371
Rohan Dey <rohandey@gmail.com>
2013-08-12 15:36:13 +0530

    event and news

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Ej::Application.routes.draw do
   match '/ipn' => 'ipn#index'
   post '/admin/members/approval'
   resources :fedex_transits
+  resources :events
 
   namespace :admin do
     post 'add_banner_photo' => 'products#add_banner_photo'

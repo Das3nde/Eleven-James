8e2d8178d45bf5fc2a33824f402555d8c679bbf8
Rohan Dey <rohandey@gmail.com>
2013-08-19 15:19:48 +0530

    commit news list

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,6 +2,12 @@ class NewsController < ApplicationController
 
   layout 'app'
 
+  def index
+    @wrapper = "marketing"
+    @news = News.order("date DESC")
+    @latest_news = News.order("date DESC").limit(2)
+  end
+
   def show
     @wrapper = "marketing"
     @news = News.find(params[:id])
